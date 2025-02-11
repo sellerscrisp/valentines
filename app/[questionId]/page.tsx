874daa40c8a1e413ab7dbb78a361c64ea2edf442
sellerscrisp <sellershcrisp@gmail.com>
2025-02-11 14:38:10 -0500

    fixed??????

--- app/[questionId]/page.tsx
+++ app/[questionId]/page.tsx
@@ -1,18 +1,16 @@
 // app/[questionId]/page.tsx
-
 import { notFound } from "next/navigation";
 import QuizSlide from "@/components/Quiz/QuizSlide";
 import { quizQuestions } from "@/data/quizQuestions";
 
-// Instead of using a custom PageProps type that only allows { questionId: string },
-// update the props type to allow params to be a Promise.
-export default async function QuestionPage({
-  params,
-}: {
-  params: { questionId: string } | Promise<{ questionId: string }>;
-}) {
-  // Await params in case it is a Promise
-  const { questionId } = await params;
+type PageProps = {
+  params: {
+    questionId: string;
+  };
+};
+
+export default async function QuestionPage({ params }: PageProps) {
+  const { questionId } = params;
   const questionData = quizQuestions[questionId];
 
   if (!questionData) {
