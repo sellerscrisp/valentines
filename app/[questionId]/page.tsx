3aa609167f85e0cbf90569eb617821f1971cf583
sellerscrisp <sellershcrisp@gmail.com>
2025-02-11 14:35:44 -0500

    fixed??

--- app/[questionId]/page.tsx
+++ app/[questionId]/page.tsx
@@ -1,16 +1,18 @@
+// app/[questionId]/page.tsx
+
 import { notFound } from "next/navigation";
 import QuizSlide from "@/components/Quiz/QuizSlide";
 import { quizQuestions } from "@/data/quizQuestions";
 
-// Define the props type expected by Next.js
-interface PageProps {
-  params: {
-    questionId: string;
-  };
-}
-
-export default async function QuestionPage({ params }: PageProps) {
-  const { questionId } = params;
+// Instead of using a custom PageProps type that only allows { questionId: string },
+// update the props type to allow params to be a Promise.
+export default async function QuestionPage({
+  params,
+}: {
+  params: { questionId: string } | Promise<{ questionId: string }>;
+}) {
+  // Await params in case it is a Promise
+  const { questionId } = await params;
   const questionData = quizQuestions[questionId];
 
   if (!questionData) {
@@ -20,7 +22,7 @@ export default async function QuestionPage({ params }: PageProps) {
   return (
     <div className="min-h-screen flex flex-col justify-start items-center p-4">
       <QuizSlide {...questionData} />
-      {/* Optionally include other components like AudioToggle */}
+      {/* Optionally include other components */}
     </div>
   );
 }
