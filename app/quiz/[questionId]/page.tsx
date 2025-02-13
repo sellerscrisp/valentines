60304466ecb0d4f6157740f29971a9ca85f506db
sellerscrisp <sellershcrisp@gmail.com>
2025-02-13 12:40:15 -0500

    migrated quiz to own app route

--- app/quiz/[questionId]/page.tsx
+++ app/quiz/[questionId]/page.tsx
@@ -1,7 +1,9 @@
 // app/[questionId]/page.tsx
 import { notFound } from "next/navigation";
-import QuizSlide from "@/components/Quiz/QuizSlide";
+import QuizSlide from "../components/QuizSlide";
 import { quizQuestions } from "@/data/quizQuestions";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 // Now declare that params is a Promise resolving to an object with questionId.
 type PageProps = {
@@ -19,6 +21,11 @@ export default async function QuestionPage({ params }: PageProps) {
 
   return (
     <div className="min-h-screen flex flex-col justify-start items-center p-4">
+      <Link href="/" passHref>
+        <Button variant="link">
+          Home
+        </Button>
+      </Link>
       <QuizSlide {...questionData} />
       {/* Optionally include other components */}
     </div>
