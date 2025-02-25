9b8a04fea900cfcc777db1a81e44cdea0d5f385a
sellerscrisp <sellershcrisp@gmail.com>
2025-02-25 18:04:51 -0500

    Add viewport meta tag to prevent user scaling in Scrapbook layout

--- app/scrapbook/layout.tsx
+++ app/scrapbook/layout.tsx
@@ -5,7 +5,8 @@ export default function ScrapbookLayout({
 }) {
   return (
     <>
-        {children}
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+      {children}
     </>
   );
 } 
