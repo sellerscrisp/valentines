76f3b7e70a4d7da25dd899323e232ba82748564a
sellerscrisp <sellershcrisp@gmail.com>
2025-02-25 18:02:06 -0500

    Disable user scaling in viewport meta configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
       </head>
       <body className={`${inter.className} overflow-x-hidden bg-background`}>
         <main>
